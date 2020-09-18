07a00d309407a476d2814c9c0068a02e5f58c808
Kian Salehi <kiansa19@gmail.com>
2020-09-18 11:34:39 -0400

    Updated

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+from time import sleep
+#Cascade path and face cascade
+cascPath = 'haarcascade_frontalface_default.xml'
+faceCacade = cv2.CascadeClassifier(cascPath)
+video_capture = cv2.VideoCapture(0)
+while True:
+    if not video_capture.open():
+        print("Could not open the camera")
+        sleep(5)
+        pass
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCacade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30,30),
+    )
+    # Draw around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
